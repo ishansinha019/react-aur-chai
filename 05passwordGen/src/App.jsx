7af4f91edefb951995107887f835cc489b6b7c23
Ishan Sinha <ishan.sinha@siemensgamesa.com>
2025-08-28 14:43:39 +0530

    28th Aug

--- 05passwordGen/src/App.jsx
+++ 05passwordGen/src/App.jsx
@@ -1,91 +1,191 @@
-import { useState } from 'react'
-import { useCallback, useEffect, useRef } from 'react'
+// import { useState } from 'react'
+// import { useCallback, useEffect, useRef } from 'react'
+
+
+// function App() {
+//   const [length, setLength] = useState(8)
+//   const [numberAllowed, setNumberAllowed] = useState(false)
+//   const [charAllowed, setCharAllowed] = useState(false)
+//   const [pass, setPass] = useState("")
+
+//   // useRef hook : is used to create a mutable object which holds a .current property, it can be used to store a value that does not cause re-render when changed
+//   const passwordRef = useRef(null)
+
+//   const passwordGenerator = useCallback(() => {               //useCallback is used to memoize (optimize) the function so that it doesn't get recreated on every render, basically it will only be rendered when its dependencies change
+//     let pass = ""
+//     let str =
+//       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxwyz"
+//     if (numberAllowed) {
+//       str += "0123456789"
+//     }
+//     if (charAllowed) {
+//       str += "!@#$%^&*()_+"
+//     }
+//     for (let i = 1; i <= length; i++) {
+//       let character = Math.floor(Math.random() * str.length + 1)
+//       pass += str.charAt(character)
+//     }
+//     setPass(pass)
+
+
+//   }, [length, numberAllowed, charAllowed, setPass])
+
+//   const copyPasswordToClipboard = useCallback(() => {
+//     passwordRef.current?.select() // focus the input element if it exists
+//     // passwordRef.current?.setSelectionRange(0,3)   // select a range from the given output box
+
+//     window.navigator.clipboard.writeText(pass)
+//   }, [pass])
+
+
+
+//   useEffect(() => {                                          //useEffect is used to perform side effects in function components, it runs after the render is committed to the screen
+//     passwordGenerator()
+//   }, [length, numberAllowed, charAllowed, passwordGenerator])
+//   return (
+//     <>
+
+//       <div className='w-full max-w-md  not-[]:mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700 '>
+//         <h1 className='text-white text-center my-3'>Password Generator</h1>
+//         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+//           <input
+//             type="text"
+//             value={pass}
+//             className='outline-none w-full py-1 px-3'
+//             placeholder='password'
+//             readOnly
+//             ref={passwordRef} // attaching the ref to the input element
+//           />
+//           <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+//         </div>
+//         <div className='flex text-sm gap-x-2'>
+//           <div className='flex items-center gap-x-1'>
+//             <input type="range" min={6} max={100} value={length} className='cursor-pointer'
+//               onChange={(e) => setLength(e.target.value)} />
+//             <label>Length: {length}</label>
+//           </div>
+//           <input
+//             type="checkbox"
+//             defaultChecked={numberAllowed}
+//             id='numberInput'
+//             onChange={() => {
+//               setNumberAllowed((prev) => !prev)
+//             }}
+//           />
+//           <label htmlFor='numberInput'>Numbers</label>
+//           <input
+//             type="checkbox"
+//             defaultChecked={charAllowed}
+//             id='charInput'
+//             onChange={() => {
+//               setCharAllowed((prev) => !prev)
+//             }}
+//           />
+//           <label htmlFor='charInput'>Characters</label>
+//         </div>
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+import { useState, useCallback, useEffect, useRef } from 'react'
+
 
 
 function App() {
   const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false)
-  const [pass, setPass] = useState("")
+  const [password, setPassword] = useState("")
 
-  // useRef hook : is used to create a mutable object which holds a .current property, it can be used to store a value that does not cause re-render when changed
+  //useRef hook
   const passwordRef = useRef(null)
 
-  const passwordGenerator = useCallback(() => {               //useCallback is used to memoize (optimize) the function so that it doesn't get recreated on every render, basically it will only be rendered when its dependencies change
+  const passwordGenerator = useCallback(() => {
     let pass = ""
-    let str =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxwyz"
-    if (numberAllowed) {
-      str += "0123456789"
-    }
-    if (charAllowed) {
-      str += "!@#$%^&*()_+"
-    }
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
     for (let i = 1; i <= length; i++) {
-      let character = Math.floor(Math.random() * str.length + 1)
-      pass += str.charAt(character)
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+
     }
-    setPass(pass)
+
+    setPassword(pass)
 
 
-  }, [length, numberAllowed, charAllowed, setPass])
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   const copyPasswordToClipboard = useCallback(() => {
-    passwordRef.current?.select() // focus the input element if it exists
-    // passwordRef.current?.setSelectionRange(0,3)   // select a range from the given output box
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
 
-    window.navigator.clipboard.writeText(pass)
-  }, [pass])
-
-
-
-  useEffect(() => {                                          //useEffect is used to perform side effects in function components, it runs after the render is committed to the screen
+  useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
-    <>
 
-      <div className='w-full max-w-md  not-[]:mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700 '>
-        <h1 className='text-white text-center my-3'>Password Generator</h1>
-        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-40 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-6 text-2xl'>Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          onClick={copyPasswordToClipboard}
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
           <input
-            type="text"
-            value={pass}
-            className='outline-none w-full py-1 px-3'
-            placeholder='password'
-            readOnly
-            ref={passwordRef} // attaching the ref to the input element
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e) => { setLength(e.target.value) }}
           />
-          <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+          <label>Length: {length}</label>
         </div>
-        <div className='flex text-sm gap-x-2'>
-          <div className='flex items-center gap-x-1'>
-            <input type="range" min={6} max={100} value={length} className='cursor-pointer'
-              onChange={(e) => setLength(e.target.value)} />
-            <label>Length: {length}</label>
-          </div>
+        <div className="flex items-center gap-x-1">
           <input
             type="checkbox"
             defaultChecked={numberAllowed}
-            id='numberInput'
+            id="numberInput"
             onChange={() => {
-              setNumberAllowed((prev) => !prev)
+              setNumberAllowed((prev) => !prev);
             }}
           />
-          <label htmlFor='numberInput'>Numbers</label>
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
           <input
             type="checkbox"
             defaultChecked={charAllowed}
-            id='charInput'
+            id="characterInput"
             onChange={() => {
               setCharAllowed((prev) => !prev)
             }}
           />
-          <label htmlFor='charInput'>Characters</label>
+          <label htmlFor="characterInput">Characters</label>
         </div>
       </div>
+    </div>
 
-    </>
   )
 }
 
--- 06currencyConv/src/components/InputBox.jsx
+++ 06currencyConv/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React, {useId} from "react";
+import React, {useId} from 'react'
 
 function InputBox({
     label,
@@ -9,42 +9,40 @@ function InputBox({
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,
-
     className = "",
-
 }) {
-    const amountInputId = useId()
+   const amountInputId = useId()
+
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`p-3 rounded-lg text-sm flex ${className} bg-white/30`}>
             <div className="w-1/2">
-                <label htmlFor= {amountInputId} className="text-black/40 mb-2 inline-block">
-                    {label} 
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block ">
+                    {label}
                 </label>
                 <input
                     id={amountInputId}
-                    className="outline-none w-full bg-transparent py-1.5"
-                    type="number"
+                    className="outline-none w-full bg-transparent py-1.5 text-black"
+                    type="text"
                     placeholder="Amount"
                     disabled={amountDisable}
                     value={amount}
-                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}      
-                    // use a checker to ensure onAmountChange is a function, using Number to convert input value to a number
-
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <p className="mb-2 w-full">Currency Type</p>
                 <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    className="rounded-lg px-1 py-1 cursor-pointer outline-none text-black"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
                 >
-                    {currencyOptions.map((currency) => (
-                        <option key={currency} value={currency}>                                       
-                            {currency.toUpperCase()}
-                        </option>
-                        // Remember to provide a key whenever using loops in jsx
-                        // The value should be the currency code or name
-                    ))}
+                    
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                            {currency}
+                            </option>
+                        ))}
                 
                 </select>
             </div>
@@ -53,5 +51,3 @@ function InputBox({
 }
 
 export default InputBox;
-
-
